2f77ac08eaee10bd61e064d83a5bf5832e9034ba
Shaan Somarchand <153312506+theTropicalBoi@users.noreply.github.com>
2025-02-24 13:19:25 +0400

    Add configuration management with environment-based settings

---
+++ config.py
@@ -0,0 +1,13 @@
+import os
+
+class Config:
+    SECRET_KEY = os.getenv('SECRET_KET', 'default_secret_key')
+    DEBUG = False
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+
+config = {
+    'devlopment': DevelopmentConfig,
+    'default': DevelopmentConfig
+}
