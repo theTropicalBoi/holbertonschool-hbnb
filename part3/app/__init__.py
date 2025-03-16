40bec1e989b8bf1e88dafbd53df7fdd3f830199f
Shaan Somarchand <153312506+theTropicalBoi@users.noreply.github.com>
2025-03-16 18:48:51 +0100

    Integrate Flask-JWT-Extended for JWT authentication in create_app function

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -1,11 +1,14 @@
 from flask import Flask
 from flask_restx import Api
 from flask_bcrypt import Bcrypt
+from flask_jwt_extended import JWTManager
 from app.api.v1.users import api as users_ns
 from app.api.v1.amenities import api as amenities_ns
 from app.api.v1.places import api as places_ns
 from app.api.v1.reviews import api as reviews_ns
 
+
+jwt = JWTManager()
 bcrypt = Bcrypt()
 
 def create_app(config_class="config.DevelopmentConfig"):
@@ -18,6 +21,7 @@ def create_app(config_class="config.DevelopmentConfig"):
     api.add_namespace(places_ns, path='/api/v1/places')
     api.add_namespace(reviews_ns, path='/api/v1/reviews')
 
-    bcrypt.init_app(app) #TODO - Does this line goes here ?
+    bcrypt.init_app(app)
+    jwt.init_app(app)
 
     return app
