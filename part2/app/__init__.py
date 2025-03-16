fe4a0b4726b0d9b80144b3589e604967a261b6d7
Yann11GAB <danieloubassou@gmail.com>
2025-03-16 13:15:07 +0100

    delete branch

--- part2/app/__init__.py
+++ part2/app/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_restx import Api
 
 
-def createApp():
+def create_app():
     app = Flask(__name__)
     api = Api(app, version="1.0", title="HBnB API", description="HBnB Application API", doc="/api/v1")
 
