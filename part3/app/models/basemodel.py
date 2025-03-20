32e7eaed83202ca43b9bfe2f5173c7abf65ace7b
Shaan Somarchand <153312506+theTropicalBoi@users.noreply.github.com>
2025-03-20 10:52:54 +0100

    Refactor BaseModel to integrate SQLAlchemy ORM

--- part3/app/models/basemodel.py
+++ part3/app/models/basemodel.py
@@ -1,11 +1,13 @@
+from app import db
 import uuid
 from datetime import datetime
 
-class BaseModel:
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+class BaseModel(db.Model):
+    __abstract__ = True # Ensures SQLAlchemy does not create a table for BaseModel
+
+    id = db.Column(db.String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
+    created_at = db.Column(db.DateTime, default=datetime.utcnow) #FIXME - Why does utcnow is strike ?
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
 
     def save(self):
         """Update the updated_at timestamp whenever the object is modified"""
