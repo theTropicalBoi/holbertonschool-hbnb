3570d3b6b20ac2779a9dde2a880167ee94bed296
Shaan Somarchand <153312506+theTropicalBoi@users.noreply.github.com>
2025-03-20 10:52:40 +0100

    Implement SQLAlchemy repository for data persistence and update facade to use it

--- part3/app/services/facade.py
+++ part3/app/services/facade.py
@@ -1,4 +1,4 @@
-from app.persistence.repository import InMemoryRepository
+from app.persistence.repository import SQLAlchemyRepository
 from app.models.user import User
 from app.models.amenity import Amenity
 from app.models.place import Place
@@ -6,17 +6,17 @@ from app.models.review import Review
 
 class HBnBFacade:
     def __init__(self):
-        self.user_repo = InMemoryRepository()
-        self.amenity_repo = InMemoryRepository()
-        self.place_repo = InMemoryRepository()
-        self.review_repo = InMemoryRepository()
+        self.user_repo = SQLAlchemyRepository(User)
+        self.amenity_repo = SQLAlchemyRepository(Amenity)
+        self.place_repo = SQLAlchemyRepository(Place)
+        self.review_repo = SQLAlchemyRepository(Review)
 
     # USER
     def create_user(self, user_data):
         user = User(**user_data)
         self.user_repo.add(user)
         return user
-    
+
     def get_users(self):
         return self.user_repo.get_all()
 
