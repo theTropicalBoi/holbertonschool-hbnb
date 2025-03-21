f3c518258980b85d5b7018a041808aee35e595b6
Shaan Somarchand <153312506+theTropicalBoi@users.noreply.github.com>
2025-03-21 11:11:46 +0100

    I dont know anymore

--- part3/app/api/v1/users.py
+++ part3/app/api/v1/users.py
@@ -1,7 +1,7 @@
 from flask_restx import Namespace, Resource, fields
 from flask import request
 from app.services import facade
-from flask_jwt_extended import jwt_required, get_jwt_identity, get_jwt
+from flask_jwt_extended import jwt_required, get_jwt_identity
 
 api = Namespace('users', description='User operations')
 
